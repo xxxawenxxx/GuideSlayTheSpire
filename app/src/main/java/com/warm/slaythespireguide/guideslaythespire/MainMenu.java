e11342b4cea4a8f7cefcdfc7fb3bb465dad637f8
xxxawenxxx <awenm10@gmail.com>
2019-06-20 01:52:21 +0700

    Merge branch 'master' of https://github.com/xxxawenxxx/GuideSlayTheSpire
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/vcs.xml

--- app/src/main/java/com/warm/slaythespireguide/guideslaythespire/MainMenu.java
+++ app/src/main/java/com/warm/slaythespireguide/guideslaythespire/MainMenu.java
@@ -1,14 +1,48 @@
 package com.warm.slaythespireguide.guideslaythespire;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
+
+    CardView Character_button;
+    CardView Ascension_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        Character_button = (CardView) findViewById(R.id.Character_button);
+        Ascension_button = (CardView) findViewById(R.id.Ascension_button);
+
         // TEST COMMIT
+
+        Character_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCharacter();
+            }
+        });
+
+        Ascension_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAscensions();
+            }
+        });
     }
+
+    public void openCharacter(){
+        Intent intent = new Intent(this, Characters.class);
+        startActivity(intent);
+    }
+    public void openAscensions(){
+        Intent intent1 = new Intent(this, Characters.class);
+        startActivity(intent1);
+    }
+
 }
