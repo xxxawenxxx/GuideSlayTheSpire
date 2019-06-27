d3b714242da5d57ceeb1b1be9cc669673762d3db
xxxawenxxx <awenm10@gmail.com>
2019-06-27 17:23:36 +0700

    Merge branch 'master' of https://github.com/xxxawenxxx/GuideSlayTheSpire
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/vcs.xml

---
+++ app/src/main/java/com/warm/slaythespireguide/guideslaythespire/Defect_frost_build.java
@@ -0,0 +1,13 @@
+package com.warm.slaythespireguide.guideslaythespire;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Defect_frost_build extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_defect_frost_build);
+    }
+}
