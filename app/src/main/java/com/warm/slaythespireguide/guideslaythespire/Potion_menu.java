584244d912bc03b0702eba2064c9c86c3277e955
xxxawenxxx <awenm10@gmail.com>
2019-07-02 19:34:37 +0700

    Merge branch 'master' of https://github.com/xxxawenxxx/GuideSlayTheSpire
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/vcs.xml

---
+++ app/src/main/java/com/warm/slaythespireguide/guideslaythespire/Potion_menu.java
@@ -0,0 +1,13 @@
+package com.warm.slaythespireguide.guideslaythespire;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Potion_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_potion_menu);
+    }
+}
