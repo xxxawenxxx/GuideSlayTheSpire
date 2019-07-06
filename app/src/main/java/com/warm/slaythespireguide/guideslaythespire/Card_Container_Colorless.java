bc15cb65712c265e1fa82497db97b9467eb70b5f
xxxawenxxx <awenm10@gmail.com>
2019-07-07 01:54:57 +0700

    --add card in java--

--- app/src/main/java/com/warm/slaythespireguide/guideslaythespire/Card_Container_Colorless.java
+++ app/src/main/java/com/warm/slaythespireguide/guideslaythespire/Card_Container_Colorless.java
@@ -71,6 +71,48 @@ public class Card_Container_Colorless extends AppCompatActivity {
     }
 
     private void initCardImage(ArrayList<Integer> card_colorless_image) {
-        card_colorless_image.add(R.drawable.bash);
+        card_colorless_image.add(R.drawable.apparation);
+        card_colorless_image.add(R.drawable.bite);
+        card_colorless_image.add(R.drawable.jax);
+        card_colorless_image.add(R.drawable.ritual_dagger);
+        card_colorless_image.add(R.drawable.shiv);
+        card_colorless_image.add(R.drawable.bandageup);
+        card_colorless_image.add(R.drawable.blind);
+        card_colorless_image.add(R.drawable.darkshackles);
+        card_colorless_image.add(R.drawable.deepbreath);
+        card_colorless_image.add(R.drawable.discovery);
+
+        card_colorless_image.add(R.drawable.dramaticentrance);
+        card_colorless_image.add(R.drawable.enlightenment);
+        card_colorless_image.add(R.drawable.finesse);
+        card_colorless_image.add(R.drawable.flashofsteel);
+        card_colorless_image.add(R.drawable.forethought);
+        card_colorless_image.add(R.drawable.goodinstincts);
+        card_colorless_image.add(R.drawable.impatience);
+        card_colorless_image.add(R.drawable.jackofalltrades);
+        card_colorless_image.add(R.drawable.madness);
+        card_colorless_image.add(R.drawable.mindblast);
+
+        card_colorless_image.add(R.drawable.panacea);
+        card_colorless_image.add(R.drawable.panic_button);
+        card_colorless_image.add(R.drawable.purity);
+        card_colorless_image.add(R.drawable.swiftstrike);
+        card_colorless_image.add(R.drawable.trip);
+        card_colorless_image.add(R.drawable.apotheosis);
+        card_colorless_image.add(R.drawable.chrysalis);
+        card_colorless_image.add(R.drawable.hand_of_greed);
+        card_colorless_image.add(R.drawable.magnetism);
+        card_colorless_image.add(R.drawable.masterofstrategy);
+
+        card_colorless_image.add(R.drawable.mayhem);
+        card_colorless_image.add(R.drawable.metamorphosis);
+        card_colorless_image.add(R.drawable.panache);
+        card_colorless_image.add(R.drawable.sadisticnature);
+        card_colorless_image.add(R.drawable.secrettechnique);
+        card_colorless_image.add(R.drawable.secretweapon);
+        card_colorless_image.add(R.drawable.the_bomb);
+        card_colorless_image.add(R.drawable.thinkingahead);
+        card_colorless_image.add(R.drawable.transmutation);
+        card_colorless_image.add(R.drawable.violence);
     }
 }
